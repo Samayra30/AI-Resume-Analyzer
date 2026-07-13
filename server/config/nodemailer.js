b9c7d51e16adac58f839bec44eb0daf0b9039e40
Samayra <samayrathakur129@gmail.com>
2026-07-13 11:30:07 +0530

    Update nodemailer

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -4,22 +4,13 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
-});
 
-// 👇 Paste it here
-transporter.verify((err, success) => {
-  if (err) {
-    console.log("SMTP Error:", err);
-  } else {
-    console.log("SMTP Ready");
-  }
+  family: 4, // Force IPv4
 });
 
 module.exports = transporter;
